4b9ba812d47b7572dc6c29561531ab8d3bbc579b
Patrick Lin <linpatrick11@gmail.com>
2020-04-20 19:15:09 -0400

    renamed lofai_project to LofaiProject

--- LofaiProject/lofai_project/urls.py
+++ LofaiProject/lofai_project/urls.py
@@ -1,4 +1,4 @@
-"""lofai_project URL Configuration
+"""LofaiProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
